8d276e02342a888d89713ab44045f71ff0f4e5a3
Julia-Legrand <julia-legrand@orange.fr>
2023-11-27 13:25:21 +0100

    Mise à jour du code

--- site-actualites/src/Form/RegistrationFormType.php
+++ site-actualites/src/Form/RegistrationFormType.php
@@ -4,22 +4,28 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -27,18 +33,27 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        // la longueur maximale autorisée par Symfony pour des raisons de sécurité
                         'max' => 4096,
                     ]),
                 ],
             ])
-            ->add('role')
-        ;
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Éditeur' => 'ROLE_EDITOR',
+                    'Utilisateur' => 'ROLE_USER',
+                ],
+                'multiple' => false, // Permet à l'utilisateur de sélectionner plusieurs rôles
+                'expanded' => true, // Affiche les choix comme une liste de cases à cocher
+                'required' => false, // Définissez à true si les rôles sont obligatoires
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
